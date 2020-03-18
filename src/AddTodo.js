1b23ac9113a9bd2edf5656ac620342f31df16d95
Vishad Patel <vipatel@squarespace.com>
2020-03-18 00:38:20 -0400

    adding functionality to clear input field

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -13,13 +13,16 @@ class Addtodo extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addTodo(this.state);
+        this.setState({
+            content: ''
+        })
     }
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Add new todo: </label>
-                    <input type="text" onChange={this.handleChange} />
+                    <input type="text" onChange={this.handleChange} value={this.state.content} />
                 </form>
 
             </div>
