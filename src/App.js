039402c7f633942a0a7ba67e61e49dbb9940a3f0
Vishad Patel <vipatel@squarespace.com>
2020-03-20 23:05:12 -0400

    adding feature to import data from api's and some visiual betterment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import Navbar from './components/Navbar';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 import TodoAdd from './TodoAdd'
+import Post from './components/Post'
 
 class App extends Component {
 
@@ -13,10 +14,13 @@ class App extends Component {
       <BrowserRouter>
         <Navbar />
         <div className="todo-app container">
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-          <Route path='/todo' component={TodoAdd} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+            <Route path='/todo' component={TodoAdd} />
+            <Route path="/:post_id" component={Post} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
